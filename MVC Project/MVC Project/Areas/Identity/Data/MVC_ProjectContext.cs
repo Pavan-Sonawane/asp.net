4d771e7ff07ee300c9e808b055a41b75077de98d
Pavan Sonawane <101343497+pavansonawane2266@users.noreply.github.com>
2023-11-25 17:41:49 +0530

    Success

---
+++ MVC Project/MVC Project/Areas/Identity/Data/MVC_ProjectContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Project.Data;
+
+public class MVC_ProjectContext : IdentityDbContext<IdentityUser>
+{
+    public MVC_ProjectContext(DbContextOptions<MVC_ProjectContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
